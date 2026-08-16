c15b280a0157f71e057996414b77564df1c78c3b
Lakshya13-09 <lakshyajha1309@gmail.com>
2026-08-16 12:23:33 +0530

    Time: 3 ms (89.42%) | Memory: 25.8 MB (48.03%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                ans[i] = 0;
+            }
+            else{
+                long long index = (i + (long long)nums[i]) % n;
+                index = (index + n) % n;
+                ans[i] = nums[index];
+            }
+        }
+        return ans;
+    }
+};
